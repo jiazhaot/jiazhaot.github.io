bf3bc1070c645473179e9b0e9cda44b93fc9f869
jiazhaot <jiazhaot@student.unimelb.edu.au>
2022-06-07 01:19:00 +1000

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,16 @@
-// search function 
-// it does not work.
-var els = document.querySelectorAll( '.headerright a' );
-var searchForm = document.querySelector( '.headersearch' );
-els.forEach( function( el ) {
-  el.onclick = function() {
-    if ( el.classList.contains( 'searchicon' ) ) {
-      searchForm.children[0].focus(); 
-      if ( searchForm.style.opacity == 0 ) { 
-        searchForm.style.opacity = 1;
-      } else {
-        searchForm.style.opacity = 0;
-      }    
-    } 
-   }
-})
+// close search bar
+window.onload = function (ev){
+  var close = document.querySelector('.close');
 
+  close.onclick = function (ev1) {
+    this.parentElement.style.opacity = '0';
+  }
+}
+
+// open search bar
+const search = document.getElementsByClassName("searchicon");
+var show = document.getElementsByClassName("headersearch");
+search[0].addEventListener('click', function(){
+  show[0].style.opacity = "1";
+})
 
